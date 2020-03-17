7efce89c50b2af77ee15f627ffde05539985294c
Bruno Fitas <brunofitas@mintaka>
2020-03-17 20:42:27 +0000

    Ghost deleted

---
+++ install_update.sh
@@ -0,0 +1,28 @@
+
+# Installing flux
+helm repo add fluxcd https://charts.fluxcd.io
+
+kubectl apply -f https://raw.githubusercontent.com/fluxcd/helm-operator/master/deploy/crds.yaml
+
+kubectl create namespace flux || true
+
+# https://github.com/fluxcd/flux/tree/master/chart/flux
+helm upgrade -i flux fluxcd/flux \
+--set git.url=git@github.com:brunofitas/cluster \
+--set git.branch=master \
+--namespace flux
+
+helm upgrade -i helm-operator fluxcd/helm-operator \
+--set helm.versions=v3 \
+--set git.ssh.secretName=flux-git-deploy \
+--namespace flux
+
+# deploy key to be pasted on github
+fluxctl identity --k8s-fwd-ns flux
+
+# tell fluxctl the namespace where flux is installed
+export FLUX_FORWARD_NAMESPACE=flux
+
+# sync
+fluxctl sync
+
