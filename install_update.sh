c1365fed8c31b81b944bb40919ad625dbea4a207
Bruno Fitas <brunofitas@mintaka>
2020-03-18 01:21:35 +0000

    flux change demo

--- install_update.sh
+++ install_update.sh
@@ -7,7 +7,6 @@ kubectl apply -f https://raw.githubusercontent.com/fluxcd/helm-operator/master/d
 kubectl create namespace flux || true
 
 # https://github.com/fluxcd/flux/tree/master/chart/flux
-
 helm upgrade -i flux fluxcd/flux \
 --set git.url=git@github.com:brunofitas/cluster \
 --set git.branch=master \
@@ -15,13 +14,15 @@ helm upgrade -i flux fluxcd/flux \
 --set prometheus.enabled=true \
 --namespace flux
 
+# deploy key to be pasted on github
+fluxctl identity --k8s-fwd-ns flux
+
+
 helm upgrade -i helm-operator fluxcd/helm-operator \
 --set helm.versions=v3 \
 --set git.ssh.secretName=flux-git-deploy \
 --namespace flux
 
-# deploy key to be pasted on github
-fluxctl identity --k8s-fwd-ns flux
 
 # tell fluxctl the namespace where flux is installed
 export FLUX_FORWARD_NAMESPACE=flux
